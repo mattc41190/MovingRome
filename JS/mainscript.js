d981c46f2255adecc7cd976181fcf6ce228a4cbc
Matthew Cale <mattc41190@gmail.com>
2015-02-18 08:07:38 -0600

    commit before work

--- JS/mainscript.js
+++ JS/mainscript.js
@@ -20,33 +20,34 @@ $(document).ready(function() {
 $('input[type="range"]').rangeslider({
     polyfill: false,
 });
+
 $(document).on('input', 'input[type="range"]', function(e){
-value = e.target.value;
-circleNum = value;
-document.getElementById("dateShower").setAttribute("value", dates[value]);
-slideChanged(value);
-});
+  value = e.target.value;
+  circleNum = value;
+  document.getElementById("dateShower").setAttribute("value", dates[value]);
+  slideChanged(value);
+  });
 
 
-function slideChanged(value) {
-    if(value >= destinations1.length)
-    {
-    while(value >= destinations1.length) {
-            var lengthVal  = destinations1.length;
-            destinations1.push(pompey.locations[lengthVal])
-            destinations2.push(ceasar.locations[lengthVal])
-            destinations3.push(plots3[lengthVal])
-            destinations4.push(tiberius[lengthVal])
-            }
-    }
-    else if (value < destinations1.length) {
-        while(value < destinations1.length -1) {
-            destinations1.pop()
-            destinations2.pop()
-            destinations3.pop()
-            destinations4.pop()
-        }
-    }
+  function slideChanged(value) {
+      if(value >= destinations1.length)
+      {
+      while(value >= destinations1.length) {
+              var lengthVal  = destinations1.length;
+              destinations1.push(pompey.locations[lengthVal])
+              destinations2.push(ceasar.locations[lengthVal])
+              destinations3.push(plots3[lengthVal])
+              destinations4.push(tiberius[lengthVal])
+              }
+      }
+      else if (value < destinations1.length) {
+          while(value < destinations1.length -1) {
+              destinations1.pop()
+              destinations2.pop()
+              destinations3.pop()
+              destinations4.pop()
+          }
+      }
 }
 
 
