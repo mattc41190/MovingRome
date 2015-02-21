d0985b3e5a7291f17f7bc2f00023b01af9f619fb
Matthew Cale <mattc41190@gmail.com>
2015-02-21 13:54:38 -0600

    Folder restructure
    Made the navigation form the folder view more readable and made updates
    to coord tool JS

--- CoordinateTool/CoordinateToolJS/mainscript.js
+++ CoordinateTool/CoordinateToolJS/mainscript.js
@@ -1,3 +1,4 @@
+// Create all vars current needed in global scope
 var poly, map;
 var markers = [];
 var rowButtons = [];
@@ -6,7 +7,7 @@ var destinations = new google.maps.MVCArray();
 var placingIntermediateMarker = false;
 var entryPoint = destinations.getLength();
 
-
+// Create the map and initalize the options for the map
 function initialize() {
   var mapOptions = {
     zoom: 5,
@@ -25,21 +26,26 @@ function initialize() {
   poly = new google.maps.Polyline(polyOptions);
   poly.setMap(map);
 
-  // Add a listener for the click event
+// Add a listener for the click event
   google.maps.event.addListener(map, 'click', addLatLng);
 }
 
+// Bulk of this application belongs on the addLatLng function as all individual listners are declared upon creation
+
 function addLatLng(event) {
-// Add new <tr> to #latLongList and addListner to it
+// Create local variables
   var stringLatLng = event.latLng.toString();
   var currentRowButtonId;
 
+// Log current index at which new points will be placed
   console.log("The current entry point is: " + entryPoint);
 
-  function changeTruth(element)
+// Change the point of entry for a new index allowing the user to place a point inbetween two previously existing points
+  function changeEntry(element)
   {
+// Log the name of the button we click
     console.log(element.id)
-    placingIntermediateMarker = true;
+// Loop through all existing rowButtons, disable all other buttons, redefine entryPoint to be the index we want to insert at
     for(i in rowButtons)
     {
       if (rowButtons[i] != element.id)
@@ -49,34 +55,35 @@ function addLatLng(event) {
       else {
         entryPoint = i;
         console.log("The value of the entry point is now set to : " + entryPoint)
+        console.log("I am about to create a button not in order at index: "+ entryPoint);
       }
     }
   }
 
-
+// Loop through all table rows and assign currentButtonRowId to be current entryPoint
   for (var i = 0; i <= dateLocationsOutput.length; i++ )
   {
-    if(i == dateLocationsOutput.length)
+    if(i == entryPoint)
     {
-      console.log(i);
       currentRowButtonId = "rowBtn" + i;
-      rowButtons.push(currentRowButtonId);
+// Add button to the entryPoint index of the rowButtons array
+      rowButtons.splice(i, 0, currentRowButtonId);
     }
   }
 
-  dateLocationsOutput.push("<tr><td class='danger'> <button id='"+currentRowButtonId+
+// Add new table row and give each button it's own listener
+  dateLocationsOutput.splice(entryPoint, 0,"<tr><td class='danger'> <button id='"+currentRowButtonId+
     "'class='btn btn-primary'><span class='caret'></span></button></td><td class='active' >DateArea: "+stringLatLng+
     "</td></tr>");
+    document.getElementById('latLongList').innerHTML = "";
   document.getElementById('latLongList').innerHTML = dateLocationsOutput.join("");
   for(i in dateLocationsOutput)
   {
-    document.getElementById(rowButtons[i]).addEventListener('click', function(){changeTruth(this)});
+    document.getElementById(rowButtons[i]).addEventListener('click', function(){changeEntry(this)});
   }
 
 // Add new polyline point at point of 'click' event
   destinations.insertAt(entryPoint, event.latLng);
-  entryPoint = destinations.getLength();
-
 
 // Add a new marker at the new plotted point on the polyline.
   var marker = new google.maps.Marker({
@@ -85,8 +92,9 @@ function addLatLng(event) {
     map: map,
     draggable: true
   });
-  markers.push(marker);
+  markers.splice(entryPoint, 0, marker);
 
+// Delete Marker function
 // Add a 'click' listener to this newly placed marker (every marker gets its own click listener)
   google.maps.event.addListener(marker, 'click', function() {
     marker.setMap(null);
@@ -99,11 +107,12 @@ function addLatLng(event) {
       document.getElementById('latLongList').innerHTML = dateLocationsOutput.join("");
       for(i in dateLocationsOutput)
       {
-        document.getElementById(rowButtons[i]).addEventListener('click', function(){changeTruth(this)});
+        document.getElementById(rowButtons[i]).addEventListener('click', function(){changeEntry(this)});
       }
     }
   });
 
+// Move Marker function
 // Add a 'dragend' listener to this newly placed marker (every marker gets its own dragend listener)
   google.maps.event.addListener(marker, 'dragend', function() {
     var i = markers.indexOf(marker);
@@ -116,12 +125,14 @@ function addLatLng(event) {
     document.getElementById('latLongList').innerHTML = dateLocationsOutput.join("");
     for(i in dateLocationsOutput)
     {
-      document.getElementById(rowButtons[i]).addEventListener('click', function(){changeTruth(this)});
+      document.getElementById(rowButtons[i]).addEventListener('click', function(){changeEntry(this)});
     }
     }
   );
+// Set entry point to destinations.getLength() so that we have a clean slate to start with.
+  entryPoint = destinations.getLength();
 }
-// addLatLng function ends here
+// addLatLng function ends here, it's a monster I know.
 
 
 // Create a method of clearing map of markers
@@ -136,11 +147,11 @@ function clearAll()
 {
   setAllMap(null);
   markers = [];
+  rowButtons = [];
   dateLocationsOutput = [];
   destinations.clear();
+  entryPoint = destinations.getLength();
   document.getElementById('latLongList').innerHTML = dateLocationsOutput;
 }
-
-
 
 google.maps.event.addDomListener(window, 'load', initialize);
